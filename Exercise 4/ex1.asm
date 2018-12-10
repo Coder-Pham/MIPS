7609e11b29dd958fcc0936e9f32e383d9ab787d7
Phạm Trọng Nhân <tendaigom@gmail.com>
2018-12-10 13:40:32 +0700

    Update assignment

---
+++ Exercise 4/ex1.asm
@@ -0,0 +1,53 @@
+# Bubble sort - descending order
+.data
+nums:   .word 10
+elems:  .word 5, 2, 3, 4, 9, 12, 14, 20, 1, 1
+space:  .asciiz " "
+
+.text
+main:
+    la $s1, elems
+    lw $s0, nums
+# s2 is i
+# s3 is j
+# i = 0 -> n-1
+subi $a1, $s0, 1    # n - 1
+loopi:  sll $t0, $s2, 2
+    beq $s2, $a1, Exit
+
+    add $s3, $0, $0
+    sub $a0, $s0, $s2
+    subi $a0, $a0, 1    # n-i-1
+    add $s2, $s2, 1
+
+    # i = 0 -> n-i-1
+    loopj:  sll $t1, $s3, 2
+            add $t1, $t1, $s1
+            lw $s4, 0($t1)
+            lw $s5, 4($t1)
+            blt	$s4, $s5, swap	# if $s4 < $s5 then swap
+done_swap:  add $s3, $s3, 1  
+	    beq $a0, $s3, loopi
+            j loopj
+swap:   addi $t2, $s4, 0
+        sw $s5, 0($t1)
+        sw $t2, 4($t1)
+        j done_swap
+Exit:
+
+addi $s2, $0, 0
+Loop_result:    sll $t0, $s2, 2
+    add $t0, $t0, $s1
+    # Print value
+    lw $a0, 0($t0)
+    li $v0, 1
+    syscall
+    # print space
+    li $v0, 4
+    la $a0, space
+    syscall
+    # Branch if count equals
+    addi $s2, $s2, 1
+    beq $s2, 10, Exit_result
+    j Loop_result
+Exit_result:
---
+++ Exercise 4/ex2.asm
@@ -0,0 +1,123 @@
+# QuickSort - ascending order
+.data
+nums:   .word 15
+elems:  .word 17, 24, 19, 21, 14, 16, 18, 12, 22, 23, 11, 4, 3, 14, 21
+space:  .asciiz " "
+
+.text
+.globl main
+main:
+    la $s1, elems
+    lw $s0, nums
+    # Use stack to store HEAD - TAIL SUBARRAY
+    addi $a0, $0, 0
+    addi $a1, $s0, -1
+    jal quicksort   # Call quicksort
+    # Print
+    addi $s2, $0, 0
+    j Print
+
+quicksort:  
+    # $a0 = head
+    # $a1 = tail
+    addi $sp, $sp, -24
+    sw $a0, 0($sp)      # Head
+    sw $a1, 4($sp)      # Tail
+    sw $t0, 8($sp)      # i
+    sw $t1, 12($sp)     # j
+    sw $t2, 16($sp)     # Median
+    sw $ra, 20($sp)
+
+    # Set i and j
+    move $t0, $a0
+    move $t1, $a1
+
+    # Median position - (i + j) / 2
+    add $t2, $t0, $t1
+    addi $t3, $0, 2
+    div		$t2, $t3			# $t2 / 2
+    mflo	$t2				# $t2 = floor($t2 / 2) 
+    # Median 
+    sll $t2, $t2, 2
+    add $t2, $t2, $s1
+    lw $t2, 0($t2)
+
+    # While (i < j)
+    i_j:bge $t0, $t1, recurse1
+        # while (a[i] < mid)    i++
+        i_mid:  # Get a[i] -> $t3
+            sll $t3, $t0, 2
+            add $t3, $t3, $s1
+            lw $t3, 0($t3)
+            # a[i] < mid
+            bge $t3, $t2, j_mid
+            addi $t0, $t0, 1
+            j i_mid
+
+        # While (a[j] > mid)    j--
+        j_mid:  # Get a[j] -> $t4
+            sll $t4, $t1, 2
+            add $t4, $t4, $s1
+            lw $t4, 0($t4)
+            # a[j] > mid
+            ble $t4, $t2, swap 
+            addi $t1, $t1, -1
+            j j_mid
+
+        # Swap a[i] - a[j]
+        swap:   bgt $t0, $t1, i_j
+            # $t3 = a[i]
+            # $t4 = a[j]
+            # a[i] = a[j]
+            sll $t5, $t0, 2
+            add $t5, $t5, $s1
+            sw $t4, 0($t5)
+            # a[j] = temp = old a[i]
+            sll $t5, $t1, 2
+            add $t5, $t5, $s1
+            sw $t3, 0($t5)
+            # i++      j--
+            addi $t0, $t0, 1
+            addi $t1, $t1, -1
+    j i_j
+
+recurse1:
+    # If head < j
+    bge	$a0, $t1, recurse2	# if $a0 >= $t1 then recurse2
+        move $a1, $t1
+        jal quicksort
+        # pop stack
+        lw $a0, 0($sp)
+        lw $a1, 4($sp)
+        lw $ra, 20($sp)
+
+recurse2:
+    # If i < tail
+    bge $t0, $a1, Exit
+        move $a0, $t0
+        jal quicksort
+        # pop stack
+        lw $a0, 0($sp)
+        lw $a1, 4($sp)
+        lw $ra, 20($sp)
+
+Exit:
+    lw $t0, 8($sp)
+    lw $t1, 12($sp)
+    lw $t2, 16($sp)
+    addi $sp, $sp, 24
+    jr	$ra				# jump to stack
+
+Print:  sll $t0, $s2, 2
+    add $t0, $t0, $s1
+    lw $a0, 0($t0)
+    li $v0, 1
+    syscall
+    # Print space
+    li $v0, 4
+    la $a0, space
+    syscall
+    addi $s2, $s2, 1
+    beq $s2, 15, Exit_print
+    j Print
+Exit_print:
